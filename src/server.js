f91c4427e813d5bcc3a1345d52e8771cb8ba0f0f
Beatriz Monteiro <biafigmonteiro@gmail.com>
2025-04-08 17:02:55 -0300

    dependencies and initial config

---
+++ src/server.js
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+import express from "express";
+import { router } from "./routes/index.js";
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(router);
+
+const PORT = process.env.PORT || 8888;
+
+app.listen(PORT, () => {
+    console.log("Running server");
+})
