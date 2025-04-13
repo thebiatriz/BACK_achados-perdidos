47f8312eaec228d7f0687cca132a72c762311c7b
Beatriz <biafigmonteiro@gmail.com>
2025-04-13 17:33:47 -0300

    install and add cors (#6)

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 import dotenv from "dotenv";
 import express from "express";
 import { router } from "./routes/index.js";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(router);
 
 const PORT = process.env.PORT || 8888;
