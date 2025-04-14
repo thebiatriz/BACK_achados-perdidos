f99ed3e7453e7f781f2a70745a9a0cab8804f960
Beatriz Monteiro <biafigmonteiro@gmail.com>
2025-04-13 21:24:43 -0300

    refactor item and adjust

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,14 @@
-import { Router } from 'express';
-import itemRoutes from './itemRoutes.js';
+import { Router } from "express";
 import categoryRoutes from "./categoryRoutes.js";
 import userRoutes from "./userRoutes.js";
 import loginRoutes from "./loginRoutes.js";
+import itemRoutes from "./itemRoutes.js";
 
 const router = Router();
 
 router.use("/api/categorias", categoryRoutes);
 router.use("/api/usuarios", userRoutes);
 router.use("/api/login", loginRoutes);
-
-router.use('/api/itens', itemRoutes);
+router.use("/api/itens", itemRoutes);
 
 export { router };
